a106e6c0d549f2c3827266886cd2b2711bfa2eda
wangqian <wangqian@wakedata.com>
2019-09-11 17:33:11 +0800

    Revert "Revert "commit helloworld""
    This reverts commit 831e1025

--- src/main/java/com/chart/wq/TestController.java
+++ src/main/java/com/chart/wq/TestController.java
@@ -9,10 +9,30 @@ import java.util.Map;
 @RestController
 public class TestController {
 
+    private String name;
+
     @Autowired
     private TestBusinessHolder testBusinessHolder;
     @RequestMapping("/hello")
-    public Map<String, TestBusiness> test(){
+    public Map<String, TestBusiness> tests(){
+        Map<String, TestBusiness> stringTestBusinessMap = testBusinessHolder.get();
+        return stringTestBusinessMap;
+    }
+
+    @RequestMapping("/yesj")
+    public String getNum(){
+
+        return "helloWorld";
+    }
+
+    @RequestMapping("/yesj")
+    public String tesFile(){
+
+        return "helloWorld";
+    }
+
+    @RequestMapping("/hello")
+    public Map<String, TestBusiness> hahh(){
         Map<String, TestBusiness> stringTestBusinessMap = testBusinessHolder.get();
         return stringTestBusinessMap;
     }
